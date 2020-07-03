e5a418cf910251a4c71438aa7e4d36e2e80312ce
Michael Li <limichael89@gmail.com>
2020-07-02 22:20:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
+import MapGL, {NavigationControl} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const mapbox_token = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const navControlStyle = {
+  position: 'absolute',
+  bottom: 20,
+  right: 0,
+  padding: '10px'
+};
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 42.3601,
+        longitude: -71.0589,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0,
+      }
+    };
+  }
+
+  render() {
+    const {viewport} = this.state;
+    return (
+      <MapGL
+        {...viewport}
+        mapboxApiAccessToken={mapbox_token}
+        width = '100vw'
+        height = '100vh'
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={viewport => this.setState({viewport})}
+        >
+        <div className="nav" style={navControlStyle}>
+          <NavigationControl/>
+        </div>
+      </MapGL>
+    );
+  }  
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map/>
   );
 }
 
